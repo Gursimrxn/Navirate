0ff5f33f004603a30289c35296e7ee14ff61071a
Gursimran Singh <sgursimranmatharu@gmail.com>
2025-02-13 01:25:00 +0530

    Remove Flask backend: delete app, routes, services, and related files; add VSCode settings for Deno

--- Flask/routes/sentiment_routes.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint
-from services.sentiment_service import analyze_comment_sentiment
-
-sentiment_bp = Blueprint("sentiment", __name__)
-
-@sentiment_bp.route("/analyze-sentiment", methods=['POST'])
-def analyze_sentiment():
-    return analyze_comment_sentiment() 
--- Flask/services/database.py
+++
@@ -1,7 +0,0 @@
-from flask_pymongo import PyMongo
-
-# Create a PyMongo instance without binding to the app immediately
-mongo = PyMongo()
-
-def init_db(app):
-    mongo.init_app(app) 
